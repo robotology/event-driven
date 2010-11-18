91b845468c98449f7b1a55b9601dbf1f3e8625c8
Francesco Rea <francesco.rea@iit.it>
2010-11-18 08:14:35 +0000

    added a module for cameras alignment in iCubX
    svn path=/trunk/iCub/contrib/src/eMorph/; revision=8901

---
+++ src/visualAligner/src/vAlignerThread.cpp
@@ -0,0 +1,94 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/* 
+ * Copyright (C) 2010 RobotCub Consortium, European Commission FP6 Project IST-004370
+ * Authors: Rea Francesco
+ * email:   francesco.rea@iit.it
+ * website: www.robotcub.org 
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License fo
+ r more details
+ */
+
+/**
+ * @file cfCollectorThread.cpp
+ * @brief Implementation of the thread (see header cfCollectorThread.h)
+ */
+
+#include <iCub/cfCollectorThread.h>
+#include <cstring>
+#include <cassert>
+
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace std;
+
+#define THRATE 10
+
+cfCollectorThread::cfCollectorThread() : RateThread(THRATE) {
+    resized=false;
+    count=0;
+}
+
+cfCollectorThread::~cfCollectorThread() {
+
+}
+
+bool cfCollectorThread::threadInit() {
+    printf("starting the thread.... \n");
+    /* open ports */
+    printf("opening ports.... \n");
+    outPort.open(getName("/image:o").c_str());
+    printf("starting the converter.... \n");
+    cfConverter=new cFrameConverter();
+    cfConverter->useCallback();
+    cfConverter->open(getName("/retina:i").c_str());
+    return true;
+}
+
+void cfCollectorThread::interrupt() {
+    outPort.interrupt();
+}
+
+void cfCollectorThread::setName(string str) {
+    this->name=str;
+    printf("name: %s", name.c_str());
+}
+
+std::string cfCollectorThread::getName(const char* p) {
+    string str(name);
+    str.append(p);
+    return str;
+}
+
+void cfCollectorThread::resize(int widthp, int heightp) {
+}
+
+void cfCollectorThread::run() {
+    count++;
+    if(outPort.getOutputCount()) {
+        ImageOf<yarp::sig::PixelMono>& outputImage=outPort.prepare();
+        if(&outputImage!=0) {
+            cfConverter->getMonoImage(&outputImage);
+            outPort.write();
+        }
+        else {
+            printf("reference to the outimage null \n");
+        }
+    }
+
+}
+
+void cfCollectorThread::threadRelease() {
+    outPort.close();
+}
+
