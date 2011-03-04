2872fef955a05f6a1770ccd16411f74c96ecf80c
Francesco Rea <francesco.rea@iit.it>
2011-03-04 15:26:43 +0000

    temporary reconstruction of the library
    svn path=/trunk/iCub/contrib/src/eMorph/; revision=10752

--- emorph_lib/src/eventBuffer.cpp
+++ emorph_lib/src/eventBuffer.cpp
@@ -74,3 +74,96 @@ bool eventBuffer::read(yarp::os::ConnectionReader& connection)
     return true;
 }
 
+// -------------------------------------------------------------------------------------------------------
+
+#include <iCub/sendingBuffer.h>
+using namespace yarp::os;
+
+sendingBuffer::sendingBuffer() {
+    packet = new char[BUFFER_SIZE];
+    size_of_the_packet=0;
+}
+
+sendingBuffer::~sendingBuffer() {
+    delete[] packet;
+}
+
+void sendingBuffer::set_data(char* i_data, int i_size) {
+    memcpy(packet, i_data, BUFFER_SIZE);
+    size_of_the_packet = i_size;
+}
+
+bool sendingBuffer::write(yarp::os::ConnectionWriter& connection) {
+    connection.appendInt(BOTTLE_TAG_LIST+BOTTLE_TAG_BLOB+BOTTLE_TAG_INT);
+    connection.appendInt(2); // four elements
+    connection.appendInt(size_of_the_packet);
+    connection.appendBlock (packet, BUFFER_SIZE);
+    connection.convertTextMode(); // if connection is text-mode, convert!
+    return true;
+}
+
+bool sendingBuffer::read(ConnectionReader& connection) {
+    connection.convertTextMode(); // if connection is text-mode, convert!
+    int tag = connection.expectInt();
+    if (tag!=BOTTLE_TAG_LIST+BOTTLE_TAG_BLOB+BOTTLE_TAG_INT)
+        return false;
+    int ct = connection.expectInt();
+    if (ct!=2)
+        return false;
+    size_of_the_packet = connection.expectInt();
+    connection.expectBlock(packet, BUFFER_SIZE);
+    return true;
+}
+
+
+// -----------------------------------------------------------------------------------------
+
+
+#include <iCub/sending_buffer.h>
+#include <yarp/os/Bottle.h>
+
+using namespace std;
+using namespace yarp::os;
+
+sendingBuffer::sendingBuffer() {
+    packet = new char[SIZE_OF_DATA];
+    size_of_the_packet=0;
+}
+
+sendingBuffer::sendingBuffer(char* i_data, int i_size) {
+    packet = new char[SIZE_OF_DATA];
+    memcpy(packet, i_data, SIZE_OF_DATA);
+    size_of_the_packet = i_size;
+}
+
+sendingBuffer::~sendingBuffer() {
+    delete[] packet;
+}
+
+void sendingBuffer::set_data(char* i_data, int i_size) {
+    memcpy(packet, i_data, SIZE_OF_DATA);
+    size_of_the_packet = i_size;
+}
+
+bool sendingBuffer::write(ConnectionWriter& connection) {
+    connection.appendInt(BOTTLE_TAG_LIST+BOTTLE_TAG_BLOB+BOTTLE_TAG_INT);
+    connection.appendInt(2); // four elements
+    connection.appendInt(size_of_the_packet);
+    connection.appendBlock (packet, SIZE_OF_DATA);
+    connection.convertTextMode(); // if connection is text-mode, convert!
+    return true;
+}
+
+bool sendingBuffer::read(yarp::os::ConnectionReader& connection) {
+    connection.convertTextMode(); // if connection is text-mode, convert!
+    int tag = connection.expectInt();
+    if (tag!=BOTTLE_TAG_LIST+BOTTLE_TAG_BLOB+BOTTLE_TAG_INT)
+        return false;
+    int ct = connection.expectInt();
+    if (ct!=2)
+        return false;
+    size_of_the_packet = connection.expectInt();
+    connection.expectBlock(packet, SIZE_OF_DATA);
+    return true;
+}
+
