d937919aef6d1a75e5f568155b0626ae2f2d52cf
Shashank Pathak <shashank.pathak@iit.it>
2011-04-01 13:52:47 +0000

    Adding compiler specific identifier which otherwise would fail the build.
    svn path=/trunk/iCub/contrib/src/eMorph/; revision=11128

--- emorph_lib/src/eventConversion.cpp
+++ emorph_lib/src/eventConversion.cpp
@@ -30,10 +30,10 @@
 using namespace std;
 using namespace yarp::os;
 
-//#define LINUX
-#ifndef LINUX
+
+#ifndef  __linux__ || linux || LINUX
 typedef unsigned long uint32_t;
-#endif // LINUX
+#endif
 
 #define MAXVALUE 114748364 //4294967295
 #define maxPosEvent 10000
