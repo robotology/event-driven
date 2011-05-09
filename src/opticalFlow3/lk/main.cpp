95bd64d61dae0df7f666e0d7495ddb7c7d7e3e5f
Alessandro Scalzo <alessandro.scalzo@iit.it>
2011-05-09 13:04:15 +0000

    No commit message
    svn path=/trunk/iCub/contrib/src/eMorph/; revision=11605

--- src/opticalFlow3/lk/main.cpp
+++ src/opticalFlow3/lk/main.cpp
@@ -103,7 +103,7 @@ int main(int argc, char* argv[])
         std::string in;
         while(true)
         {
-          yarp::os::Time::delay (0.002);
+          yarp::os::Time::delay(1.0);
           std::cin >> in;
           if (in=="quit") break;
         }
