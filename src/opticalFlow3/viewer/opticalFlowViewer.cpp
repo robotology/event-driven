467da904f6332b373f603b3eb81c24764475069f
Alessandro Scalzo <alessandro.scalzo@iit.it>
2011-05-09 12:30:33 +0000

    No commit message
    svn path=/trunk/iCub/contrib/src/eMorph/; revision=11602

--- src/opticalFlow3/viewer/opticalFlowViewer.cpp
+++ src/opticalFlow3/viewer/opticalFlowViewer.cpp
@@ -1,29 +1,54 @@
 #include "opticalFlowViewer.h"
 
-opticalFlowViewer::opticalFlowViewer(bool color)
+opticalFlowViewer::opticalFlowViewer(double gain)
 {
-    mColor=color;
+    mGain=gain;
 
-    for (int x=-32; x<=32; ++x)
+    for (int x=-128; x<128; ++x)
     {
-        for (int y=-32; y<=32; ++y)
+        for (int y=-128; y<128; ++y)
         {
             double alfa=1.5+1.5*atan2(double(y),double(x))/3.1415927;
             if (alfa>3.0) alfa=3.0; else if (alfa<0.0) alfa=0.0;
+            double mag=2.0*sqrt(double(x*x+y*y));
+            if (mag>255.0) mag=255.0;
 
             if (alfa<1.0)
             {
-                mPalette[x+32][y+32]=yarp::sig::PixelRgb((unsigned char)(255.0*alfa),(unsigned char)(255.0*(1.0-alfa)),0);
+                double dA=(255.0-mag)+mag*alfa;
+                double dB=(255.0-mag)+mag*(1.0-alfa);
+                
+                int a,b;
+                if (dA<0.0) a=0; else if (dA>255.0) a=255; else a=(int)dA;
+                if (dB<0.0) b=0; else if (dB>255.0) b=255; else b=(int)dB;
+                
+                mPalette[x+128][y+128]=yarp::sig::PixelRgb(a,b,int(255.0-mag));
             }
             else if (alfa<2.0)
             {
                 alfa-=1.0;
-                mPalette[x+32][y+32]=yarp::sig::PixelRgb((unsigned char)(255.0*(1.0-alfa)),0,(unsigned char)(255.0*alfa));
+
+                double dA=(255.0-mag)+mag*alfa;
+                double dB=(255.0-mag)+mag*(1.0-alfa);
+                
+                int a,b;
+                if (dA<0.0) a=0; else if (dA>255.0) a=255; else a=(int)dA;
+                if (dB<0.0) b=0; else if (dB>255.0) b=255; else b=(int)dB;
+
+                mPalette[x+128][y+128]=yarp::sig::PixelRgb(b,int(255.0-mag),a);
             }
             else
             {
                 alfa-=2.0;
-                mPalette[x+32][y+32]=yarp::sig::PixelRgb(0,(unsigned char)(255.0*alfa),(unsigned char)(255.0*(1.0-alfa)));
+
+                double dA=(255.0-mag)+mag*alfa;
+                double dB=(255.0-mag)+mag*(1.0-alfa);
+                
+                int a,b;
+                if (dA<0.0) a=0; else if (dA>255.0) a=255; else a=(int)dA;
+                if (dB<0.0) b=0; else if (dB>255.0) b=255; else b=(int)dB;
+
+                mPalette[x+128][y+128]=yarp::sig::PixelRgb(int(255.0-mag),a,b);
             }
         }
     }
@@ -37,11 +62,11 @@ opticalFlowViewer::opticalFlowViewer(bool color)
         }
     }
 
-    mBaseImg.resize(4*XDIM,4*YDIM);
+    mBaseImg.resize(XDIM,YDIM);
     
-    for(int x=0; x<4*XDIM; ++x)
+    for(int x=0; x<XDIM; ++x)
     {
-        for(int y=0; y<4*YDIM; ++y)
+        for(int y=0; y<YDIM; ++y)
         {
             //mBaseImg(x,y)=0xFF;
             mBaseImg(x,y)=yarp::sig::PixelRgb(255,255,255);
@@ -75,8 +100,6 @@ void opticalFlowViewer::onRead(vecBuffer& data)
     mMapVy[x][y]+=vy;
     double norm;
 
-    int hx,hy;
-
     if (timeDiff>=TNK_TIME)
     {
         mTimeStart=mTimeCurrent;
@@ -84,7 +107,6 @@ void opticalFlowViewer::onRead(vecBuffer& data)
         yarp::sig::ImageOf<yarp::sig::PixelRgb>& img=mPort.prepare();
         img=mBaseImg;
         
-        int X,Y;
         int px,py;
 
         for (int x=0; x<XDIM; ++x)
@@ -94,38 +116,25 @@ void opticalFlowViewer::onRead(vecBuffer& data)
                 vx=mMapVx[x][y];
                 vy=mMapVy[x][y];
 
-                if ((norm=vx*vx+vy*vy)>0.0)
+                if (vx!=0.0 || vy!=0.0)
                 {
-                    X=2+4*x;        
-                    Y=2+4*y;
-                    
-                    hx=X+int(vx);
-                    hy=511-Y-int(vy);
+                    vx*=mGain;
+                    vy*=mGain;
+                    norm=sqrt(vx*vx+vy*vy);
 
-                    if (mColor)
+                    if (norm>=16384.0)
                     {
-                        if (norm>1024.0)
-                        {
-                            norm=32.0/sqrt(norm);
-                            px=32+int(vx*norm);
-                            py=32+int(vy*norm);
-                        }
-                        else
-                        {
-                            px=32+int(vx);
-                            py=32+int(vy);
-                        }
-
-                        yarp::sig::draw::addSegment(img,mPalette[px][py],X,511-Y,hx,hy);
-                        yarp::sig::draw::addCircle(img,mPalette[px][py],hx,hy,2);
+                        norm=127.9/sqrt(norm);
+                        px=128+int(vx*norm);
+                        py=128+int(vy*norm);
                     }
                     else
                     {
-                        //static const yarp::sig::PixelMono black=0;
-                        static const yarp::sig::PixelRgb black(0,0,0);
-                        yarp::sig::draw::addSegment(img,black,X,511-Y,hx,hy);
-                        yarp::sig::draw::addCircle(img,black,hx,hy,2);
+                        px=128+int(vx);
+                        py=128+int(vy);
                     }
+
+                    img(x,127-y)=mPalette[px][py];
                 }
 
                 mMapVx[x][y]=mMapVy[x][y]=0.0;
