055c1187ef35f7eb5f2c495ac3b5c9fdf22a8821
Francesco Rea <francesco.rea@iit.it>
2011-05-10 09:19:25 +0000

    adding new application
    svn path=/trunk/iCub/contrib/src/eMorph/; revision=11620

---
+++ app/eMorphVisualAttention/CMakeFiles/app-eMorphApplication.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+FILE(REMOVE_RECURSE
+"CMakeFiles/app-eMorphApplication"
+"../../f-eMorphApplication"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/app-eMorphApplication.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ app/eMorphVisualAttention/CMakeFiles/app-eMorphVisualAttention.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+FILE(REMOVE_RECURSE
+"CMakeFiles/app-eMorphVisualAttention"
+"../../f-eMorphVisualAttention"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/app-eMorphVisualAttention.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
