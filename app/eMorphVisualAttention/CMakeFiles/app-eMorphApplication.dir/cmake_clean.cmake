f4020801b33e405dca2b3f4d746520584ae8a178
Francesco Rea <francesco.rea@iit.it>
2011-05-25 12:56:35 +0000

    removing unnecessary files
    svn path=/trunk/iCub/contrib/src/eMorph/; revision=11836

--- app/eMorphVisualAttention/CMakeFiles/app-eMorphApplication.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-"CMakeFiles/app-eMorphApplication"
-"../../f-eMorphApplication"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/app-eMorphApplication.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
