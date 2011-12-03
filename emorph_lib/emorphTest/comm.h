aad22fb840fea7ec3283fb70b35f37faf3d383aa
Francesco Rea <francesco.rea@iit.it>
2011-12-03 07:37:24 +0000

    test communication library
    svn path=/trunk/iCub/contrib/src/eMorph/; revision=14775

---
+++ emorph_lib/emorphTest/comm.h
@@ -0,0 +1,230 @@
+
+#ifndef __EMORPH_COMM_H__
+#define __EMORPH_COMM_H__
+
+#include <yarp/os/all.h>
+#include <string>
+#include <deque>
+
+namespace emorph
+{
+
+namespace comm
+{
+
+
+// forward declaration
+class eEventQueue;
+
+
+/**************************************************************************/
+class eEvent
+{
+protected:
+    bool valid;
+    std::string type;
+
+public:
+    eEvent() : valid(false), type("") { }
+    bool isValid()        { return valid; }
+    std::string getType() { return type;  }
+
+    virtual int getLength()=0;
+    virtual yarp::os::Bottle encode()=0;
+    virtual yarp::os::Property getContent()=0;
+
+    static bool decode(const yarp::os::Bottle &packets, eEventQueue &events);
+};
+
+
+/**************************************************************************/
+class eEventQueue : public std::deque<eEvent*>
+{
+private:
+    // copy-constructor and overaloaded "=" operator are made
+    // private on purpose to avoid the problem of dangling
+    // pointers since the destructor frees up the memory
+    // allocated for events
+    eEventQueue(const eEventQueue&);
+    eEventQueue &operator=(const eEventQueue&);
+
+public:
+    eEventQueue() { }
+    ~eEventQueue();
+};
+
+
+/**************************************************************************/
+class TimeStamp : public eEvent
+{
+protected:
+    int stamp;
+
+public:
+    TimeStamp();    
+    TimeStamp(const TimeStamp &event);
+    TimeStamp(const yarp::os::Bottle &packets, const int pos=0);
+
+    TimeStamp &operator=(const TimeStamp &event);
+    bool operator==(const TimeStamp &event);
+
+    int getStamp()                 { return stamp;      }
+    void setStamp(const int stamp) { this->stamp=stamp; }
+
+    int getLength() { return 1; }
+    yarp::os::Bottle encode();
+    yarp::os::Property getContent();
+};
+
+
+/**************************************************************************/
+class AddressEvent : public eEvent
+{
+protected:
+    int channel;
+    int polarity;
+    int x;
+    int y;
+
+public:
+    AddressEvent();    
+    AddressEvent(const AddressEvent &event);
+    AddressEvent(const yarp::os::Bottle &packets, const int pos=0);
+
+    AddressEvent &operator=(const AddressEvent &event);
+    bool operator==(const AddressEvent &event);
+
+    int getChannel()                     { return channel;          }
+    int getPolarity()                    { return polarity;         }
+    int getX()                           { return x;                }
+    int getY()                           { return y;                }
+
+    void setChannel(const int channel)   { this->channel=channel;   }
+    void setPolarity(const int polarity) { this->polarity=polarity; }
+    void setX(const int x)               { this->x=x;               }
+    void setY(const int y)               { this->y=y;               }
+
+    int getLength() { return 1; }
+    yarp::os::Bottle encode();
+    yarp::os::Property getContent();
+};
+
+
+/**************************************************************************/
+class AddressEventFeatures : public AddressEvent
+{
+protected:
+    int orientation;
+    int xFlow;
+    int yFlow;
+
+public:
+    AddressEventFeatures();
+    AddressEventFeatures(const AddressEventFeatures &event);
+    AddressEventFeatures(const yarp::os::Bottle &packets, const int pos=0);
+
+    AddressEventFeatures &operator=(const AddressEventFeatures &event);
+    bool operator==(const AddressEventFeatures &event);
+
+    int getOrientation()                       { return orientation;            }
+    int getXFlow()                             { return xFlow;                  }
+    int getYFlow()                             { return yFlow;                  }
+
+    void setOrientation(const int orientation) { this->orientation=orientation; }
+    void setXFlow(const int xFlow)             { this->xFlow=xFlow;             }
+    void setYFlow(const int yFlow)             { this->yFlow=yFlow;             }
+
+    int getLength() { return 2; }
+    yarp::os::Bottle encode();
+    yarp::os::Property getContent();
+};
+
+
+/**************************************************************************/
+class AddressEvent3D : public eEvent
+{
+protected:
+    int disparity;
+    int polarity;
+    int x;
+    int y;
+
+public:
+    AddressEvent3D();    
+    AddressEvent3D(const AddressEvent3D &event);
+    AddressEvent3D(const yarp::os::Bottle &packets, const int pos=0);
+
+    AddressEvent3D &operator=(const AddressEvent3D &event);
+    bool operator==(const AddressEvent3D &event);
+
+    int getDisparity()                     { return disparity;          }
+    int getPolarity()                      { return polarity;           }
+    int getX()                             { return x;                  }
+    int getY()                             { return y;                  }
+
+    void setDisparity(const int disparity) { this->disparity=disparity; }
+    void setPolarity(const int polarity)   { this->polarity=polarity;   }
+    void setX(const int x)                 { this->x=x;                 }
+    void setY(const int y)                 { this->y=y;                 }
+
+    int getLength() { return 1; }
+    yarp::os::Bottle encode();
+    yarp::os::Property getContent();
+};
+
+
+/**************************************************************************/
+class AddressEvent3DFeatures : public AddressEvent3D
+{
+protected:
+    int orientation;
+    int xFlow;
+    int yFlow;
+
+public:
+    AddressEvent3DFeatures();
+    AddressEvent3DFeatures(const AddressEvent3DFeatures &event);
+    AddressEvent3DFeatures(const yarp::os::Bottle &packets, const int pos=0);
+
+    AddressEvent3DFeatures &operator=(const AddressEvent3DFeatures &event);
+    bool operator==(const AddressEvent3DFeatures &event);
+
+    int getOrientation()                       { return orientation;            }
+    int getXFlow()                             { return xFlow;                  }
+    int getYFlow()                             { return yFlow;                  }
+
+    void setOrientation(const int orientation) { this->orientation=orientation; }
+    void setXFlow(const int xFlow)             { this->xFlow=xFlow;             }
+    void setYFlow(const int yFlow)             { this->yFlow=yFlow;             }
+
+    int getLength() { return 2; }
+    yarp::os::Bottle encode();
+    yarp::os::Property getContent();
+};
+
+
+///**************************************************************************/
+//class ClusterEvent : public eEvent
+//{
+//};
+//
+//
+///**************************************************************************/
+//class ClusterEvent3D : public eEvent
+//{
+//};
+//
+//
+///**************************************************************************/
+//class TimeStamp : public eEvent
+//{
+//};
+
+
+}
+
+}
+
+#endif
+
+
---
+++ emorph_lib/emorphTest/main.cpp
@@ -0,0 +1,95 @@
+
+#include <yarp/os/all.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#include "comm.h"
+
+using namespace std;
+using namespace yarp::os;
+using namespace emorph::comm;
+
+void printPacket(const Bottle &packets)
+{
+    for (int i=0; i<packets.size(); i++)
+    {
+        cout<<hex<<setw(8)<<setfill('0')
+            <<packets.get(i).asInt()<<" ";
+    }
+    cout<<endl;
+}
+
+
+int main()
+{
+    // define events to be sent
+    TimeStamp ts;
+    ts.setStamp(1235423);
+    cout<<ts.getContent().toString().c_str()<<endl;
+
+    AddressEvent ae;
+    ae.setChannel(0);
+    ae.setPolarity(1);
+    ae.setX(121);
+    ae.setY(78);
+    cout<<ae.getContent().toString().c_str()<<endl;
+
+    AddressEvent3D ae3d;
+    ae3d.setDisparity(45);
+    ae3d.setPolarity(0);
+    ae3d.setX(98);
+    ae3d.setY(53);
+    cout<<ae3d.getContent().toString().c_str()<<endl;
+
+    AddressEventFeatures aef;
+    aef.setChannel(1);
+    aef.setPolarity(0);
+    aef.setX(99);
+    aef.setY(77);
+    aef.setOrientation(56);
+    aef.setXFlow(21);
+    aef.setYFlow(19);    
+    cout<<aef.getContent().toString().c_str()<<endl;
+
+    AddressEvent3DFeatures ae3df;
+    ae3df.setDisparity(201);
+    ae3df.setPolarity(1);
+    ae3df.setX(87);
+    ae3df.setY(12);
+    ae3df.setOrientation(3);
+    ae3df.setXFlow(65);
+    ae3df.setYFlow(90);    
+    cout<<ae3df.getContent().toString().c_str()<<endl;
+
+    // encode events within packets
+    Bottle packets;
+    packets.append(ts.encode());
+    packets.append(ae.encode());
+    packets.append(ae3d.encode());
+    packets.append(aef.encode());
+    packets.append(ae3df.encode());
+    printPacket(packets);
+
+    // network comes into play here
+    // ...
+
+    // receive the packets and
+    // decode events
+    eEventQueue q;
+    if (eEvent::decode(packets,q))
+    {
+        for (size_t i=0; i<q.size(); i++)
+            cout<<q[i]->getContent().toString().c_str()<<endl;
+    }
+    else
+        cout<<"unrecognized packets!"<<endl;
+
+    // whenever q is destructed the memory
+    // allocated for the decoded events will
+    // be automatically released!
+
+    return 0;
+}
+
+
