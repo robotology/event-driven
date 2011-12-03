fe23c5eca3a5824ce56daa45d5d79d91fa440c54
Ugo Pattacini <ugo.pattacini@iit.it>
2011-12-03 14:24:32 +0000

    copyright inserted
    svn path=/trunk/iCub/contrib/src/eMorph/; revision=14781

--- emorph_lib/codecTest/main.cpp
+++ emorph_lib/codecTest/main.cpp
@@ -1,3 +1,19 @@
+/* 
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author: Ugo Pattacini
+ * email:  ugo.pattacini@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
 
 #include <yarp/os/all.h>
 #include <iostream>
