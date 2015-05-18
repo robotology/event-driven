c0985e075ebcf1e26c34233b283c3a3e6b034d47
arrenglover <arren.glover@gmail.com>
2015-05-18 12:00:21 +0200

    [arren] added module to convert from eventClusterGuass to cartesian robot coordinates - the idea to send the cartesian coordinates to demograsper - UNTESTED

---
+++ src/movement/vTrackToRobot/include/vTrackToRobot.h
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author: Arren.Glover@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+ */
+
+#ifndef __ICUB_VTRACKTOROBOT_H__
+#define __ICUB_VTRACKTOROBOT_H__
+
+#include <yarp/os/all.h>
+#include <iCub/emorph/all.h>
+#include <yarp/dev/all.h>
+
+/*//////////////////////////////////////////////////////////////////////////////
+  VBOTTLE READER/PROCESSOR
+  ////////////////////////////////////////////////////////////////////////////*/
+
+class vTrackToRobotManager : public yarp::os::BufferedPort<emorph::vBottle>
+{
+private:
+    
+    yarp::os::BufferedPort<yarp::os::Bottle> cartOutPort;
+    yarp::dev::PolyDriver gazedriver;
+    yarp::dev::IGazeControl *gazecontrol;
+
+    enum { fromgaze, fromsize, fromstereo };
+
+    int method;
+
+public:
+    
+    vTrackToRobotManager();
+
+    bool setMethod(std::string methodname);
+
+    bool open(const std::string &name);
+    void onRead(emorph::vBottle &bot);
+    void interrupt();
+    void close();
+
+};
+
+/*//////////////////////////////////////////////////////////////////////////////
+  MODULE
+  ////////////////////////////////////////////////////////////////////////////*/
+
+class vTrackToRobotModule : public yarp::os::RFModule
+{
+private:
+
+    //the event bottle input and output handler
+    vTrackToRobotManager      vTrackToRobot;
+
+
+    //robot control settings
+    yarp::dev::PolyDriver mdriver;
+    yarp::dev::IPositionControl *pc;
+    yarp::dev::IEncoders *ec;
+
+
+public:
+
+    //the virtual functions that need to be overloaded
+    virtual bool configure(yarp::os::ResourceFinder &rf);
+    virtual bool interruptModule();
+    virtual bool close();
+    virtual double getPeriod();
+    virtual bool updateModule();
+
+};
+
+
+#endif
+//empty line to make gcc happy
---
+++ src/movement/vTrackToRobot/src/vTrackToRobot.cpp
@@ -0,0 +1,169 @@
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author: Arren.Glover@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+ */
+
+#include "vTrackToRobot.h"
+
+/*//////////////////////////////////////////////////////////////////////////////
+  VBOTTLE READER/PROCESSOR
+  ////////////////////////////////////////////////////////////////////////////*/
+
+vTrackToRobotManager::vTrackToRobotManager()
+{
+
+    method = fromgaze;
+    gazecontrol = 0;
+}
+
+bool vTrackToRobotManager::setMethod(std::string methodname)
+{
+    if(methodname == "gaze") method = fromgaze;
+    if(methodname == "size") method = fromsize;
+    if(methodname == "stereo") method = fromstereo;
+
+}
+
+/******************************************************************************/
+bool vTrackToRobotManager::open(const std::string &name)
+{
+    //and open the input port
+
+    this->useCallback();
+
+    std::string inPortName = "/" + name + "/vBottle:i";
+    if(!yarp::os::BufferedPort<emorph::vBottle>::open(inPortName)) {
+        std::cerr << "Could not open: " << inPortName << std::endl;
+        return false;
+    }
+
+    std::string outPortName = "/" + name + "/Bottle:o";
+    if(!cartOutPort.open(outPortName)) {
+        std::cerr << "Could not open: " << outPortName << std::endl;
+        return false;
+    }
+
+    if(method != fromgaze) return true;
+
+    yarp::os::Property options;
+    options.put("device", "gazecontrollerclient");
+    options.put("local", "/" + name);
+    options.put("remote", "/iKinGazeCtrl");
+    gazedriver.open(options);
+    if(gazedriver.isValid()) gazedriver.view(gazecontrol);
+
+    return gazedriver.isValid();
+}
+
+void vTrackToRobotManager::interrupt()
+{
+    cartOutPort.interrupt();
+    yarp::os::BufferedPort<emorph::vBottle>::interrupt();
+}
+
+void vTrackToRobotManager::close()
+{
+    cartOutPort.close();
+    gazedriver.close();
+    if(gazecontrol) delete gazecontrol; gazecontrol = 0;
+    yarp::os::BufferedPort<emorph::vBottle>::close();
+
+}
+
+/******************************************************************************/
+void vTrackToRobotManager::onRead(emorph::vBottle &vBottleIn)
+{
+
+    emorph::vQueue::reverse_iterator qi;
+    emorph::vQueue q = vBottleIn.getSorted<emorph::ClusterEventGauss>();
+    yarp::sig::Vector px(2), x(3);
+
+    emorph::ClusterEventGauss * v =
+            q.back()->getAs<emorph::ClusterEventGauss>();
+    if(!v) return;
+
+    px[0] = v->getXCog();
+    px[1] = v->getYCog();
+    gazecontrol->get3DPoint(0, px, 1.0, x);
+
+
+    yarp::os::Bottle& BottleOut = cartOutPort.prepare();
+    BottleOut.clear();
+    //add the XYZ position
+    BottleOut.add(x[0]); BottleOut.add(x[1]); BottleOut.add(x[2]);
+    //add some buffer ints
+    BottleOut.add(0.0); BottleOut.add(0.0); BottleOut.add(0.0);
+    //flag that the object is detected
+    BottleOut.add(1.0);
+
+    std::cout << BottleOut.toString() << std::endl;
+
+
+}
+
+
+/*//////////////////////////////////////////////////////////////////////////////
+  MODULE
+  ////////////////////////////////////////////////////////////////////////////*/
+
+bool vTrackToRobotModule::configure(yarp::os::ResourceFinder &rf)
+{
+    //set the name of the module
+    std::string moduleName =
+            rf.check("name", yarp::os::Value("autoSaccade")).asString();
+    setName(moduleName.c_str());
+
+    std::string method =
+            rf.check("method", yarp::os::Value("usegaze")).asString();
+
+    vTrackToRobot.setMethod(method);
+    vTrackToRobot.open(moduleName);
+
+    return true ;
+}
+
+/******************************************************************************/
+bool vTrackToRobotModule::interruptModule()
+{
+    vTrackToRobot.interrupt();
+    yarp::os::RFModule::interruptModule();
+    return true;
+}
+
+/******************************************************************************/
+bool vTrackToRobotModule::close()
+{
+    vTrackToRobot.close();
+    yarp::os::RFModule::close();
+    return true;
+}
+
+
+
+/******************************************************************************/
+bool vTrackToRobotModule::updateModule()
+{
+    return true;
+}
+
+/******************************************************************************/
+double vTrackToRobotModule::getPeriod()
+{
+    return 1;
+}
+
+
+
+
+//empty line to make gcc happy
