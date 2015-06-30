5be1b28177af1f1092259aaee73aa21301590665
vvasco <valentina.vasco@iit.it>
2015-06-30 09:49:25 +0200

    [vvasco] added the script to visualize the surface of events

---
+++ scripts/visualisation/vSurface.m
@@ -0,0 +1,28 @@
+% Displays the surface associated to the events
+
+filename = 'corner2DVS.txt';
+events = importdata(filename);
+
+% select polarity and channel 
+sel_pol = 1;
+sel_channel = 0;
+
+% leave all events with the selected channel and polarity 
+events_left_on = events(events(:, 1) == sel_channel & events(:, 3) == sel_pol, :);
+events_left_on(:, 2) = events_left_on(:, 2) - events_left_on(1, 2);
+
+figure(1); 
+clf;
+
+vSurf = zeros(h, w);
+
+for i=1:size(events_left_on, 1)
+    % associate to each incoming event its timestamp in vSurf 
+    vSurf(events_left_on(i, 4) + 1, events_left_on(i, 5) + 1) = events_left_on(i, 2);
+    if mod(i, 100) == 0
+        mesh(vSurf);
+        drawnow;
+    end
+end
+
+    
