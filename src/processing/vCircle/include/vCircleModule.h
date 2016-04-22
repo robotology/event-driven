c74f6bc1acd27a2f6c527c4528e5052a90854bcb
arrenglover <arren.glover@gmail.com>
2016-04-22 16:47:05 +0200

    updated CMAKELIST to not require ICUB

--- src/processing/vCircle/include/vCircleModule.h
+++ src/processing/vCircle/include/vCircleModule.h
@@ -89,7 +89,7 @@
 class vCircleReader : public yarp::os::BufferedPort<emorph::vBottle>
 {
 private:
-    
+
     //output port for the vBottle with the new events computed by the module
     yarp::os::BufferedPort<emorph::vBottle> outPort;
     yarp::os::BufferedPort<yarp::os::Bottle> scopeOut;
@@ -114,7 +114,7 @@ public:
     double inlierThreshold;
     bool hough;
     double timecounter;
-    
+
     vCircleReader();
 
     bool    open(const std::string &name, bool strictness = false);
