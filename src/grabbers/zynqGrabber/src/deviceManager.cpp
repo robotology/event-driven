751d643346e8429331d71eac33cf00031a5cfcfb
Chiara Bartolozzi <chiara.bartolozzi@iit.it>
2016-10-27 14:16:41 +0200

    iit-hpu0

--- src/grabbers/zynqGrabber/src/deviceManager.cpp
+++ src/grabbers/zynqGrabber/src/deviceManager.cpp
@@ -780,7 +780,7 @@ aerDevManager::aerDevManager(std::string dev, int clockPeriod, std::string loopB
     } else if (dev == "zynq_sens")
     {
         
-        this->deviceName = "/dev/iit_hpu0";
+        this->deviceName = "/dev/iit-hpu0";
         //clockRes = 1;
     } else {
         
