646f14f89d3d7d790ea154ae9fc2e0517245ec07
root <root@machinekit.aptenodytes>
2016-12-14 15:43:19 +0100

    added debugging for address:Y

--- src/grabbers/zynqGrabber/src/device2yarp.cpp
+++ src/grabbers/zynqGrabber/src/device2yarp.cpp
@@ -127,7 +127,7 @@ void  device2yarp::run() {
     }
 
     //we need to shift some bits around to send onward
-    for(int i = 0; i < 0; i+=8) {
+    for(int i = 0; i < nBytesRead; i+=8) {
         //int *TS =  (int *)(data.data() + i);
         int *AE =  (int *)(data.data() + i + 4);
 
@@ -178,6 +178,9 @@ void  device2yarp::run() {
 		//	std::cout << " Y: " << y;
 		//	std::cout << " C: " << channel << std::endl;
 		//}
+		int temp = ((*AE) & 0x0003FC00) >> 10;
+		std::cout << temp << std::endl;
+		continue;
 
         int tempAE = *AE & 0x3FF;
         tempAE |= ((*AE & 0x0003FC00) << 1);
