06e42b513b43864633a2fcd5dc4d4556832132c6
arrenglover <arren.glover@gmail.com>
2017-05-19 15:24:33 +0200

    adding a collect and send class

--- libraries/include/iCub/eventdriven/all.h
+++ libraries/include/iCub/eventdriven/all.h
@@ -5,4 +5,5 @@
 #include "iCub/eventdriven/vWindow_basic.h"
 #include "iCub/eventdriven/vWindow_adv.h"
 #include "iCub/eventdriven/vSurfaceHandlerTh.h"
+#include "iCub/eventdriven/vCollectSend.h"
 
---
+++ libraries/include/iCub/eventdriven/vCollectSend.h
@@ -0,0 +1,62 @@
+#ifndef __VCOLLECTSEND__
+#define __VCOLLECTSEND__
+
+#include <iCub/eventdriven/vCodec.h>
+#include <iCub/eventdriven/vBottle.h>
+#include <yarp/os/all.h>
+
+namespace ev {
+
+class collectorPort : public yarp::os::RateThread
+{
+private:
+
+    vBottle filler;
+    yarp::os::BufferedPort<vBottle> sendPort;
+    yarp::os::Mutex m;
+    yarp::os::Stamp ystamp;
+
+
+
+public:
+    collectorPort() : RateThread(1.0) {}
+
+    bool open(std::string name) {
+
+        return sendPort.open(name);
+
+    }
+
+    void pushevent(event<> v, yarp::os::Stamp y) {
+
+        m.lock();
+        filler.addEvent(v);
+        ystamp = y;
+        m.unlock();
+
+    }
+
+    void run() {
+
+        if(filler.size()) {
+            vBottle &b = sendPort.prepare();
+
+            m.lock();
+            b = filler;
+            filler.clear();
+            sendPort.setEnvelope(ystamp);
+            m.unlock();
+
+        }
+
+        sendPort.write();
+
+    }
+
+};
+
+
+
+}
+
+#endif
