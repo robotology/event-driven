b02752e520ebabf6ad6b2cda1e597286ee56189e
iaxama <massimiliano.iacono@iit.it>
2017-07-11 16:53:59 +0200

    Drawing events on output image. Not mapped correctly yet

--- src/processing/vMapping/include/vMapping.h
+++ src/processing/vMapping/include/vMapping.h
@@ -9,8 +9,62 @@
 #include <yarp/sig/all.h>
 #include <iCub/eventdriven/all.h>
 
+//TODO There is a copy of this class in the autosaccade module. Find a better location
+class EventBottleManager : public yarp::os::BufferedPort<ev::vBottle> {
+private:
+    
+    //for helping with timestamp wrap around
+    ev::vtsHelper unwrapper;
+    
+    //rate counters
+    yarp::os::Semaphore mutex;
+    unsigned long int latestStamp;
+    unsigned int vCount;
+    double yRate;
+    bool isReading;
+    ev::vQueue vQueue;
+
+public:
+    
+    EventBottleManager();
+    
+    bool    open(const std::string &name);
+    
+    //this is the entry point to your main functionality
+    void    onRead(ev::vBottle &bot);
+    
+    //the getting functions of the parent class
+    unsigned long int getTime();
+    unsigned long int popCount();
+    double getEventRate() { return yRate; }
+    
+    ev::vQueue getEvents() ;
+    bool start();
+    bool stop();
+    
+};
+
+class ImagePort : public yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelBgr> > {
+
+private:
+    yarp::sig::ImageOf<yarp::sig::PixelBgr> image;
+    yarp::os::Mutex mutex;
+    bool imageReady;
+public:
+    
+    ImagePort() : imageReady(false) { this->useCallback(); };
+    virtual void onRead( yarp::sig::ImageOf<yarp::sig::PixelBgr> &inImg );
+    yarp::sig::ImageOf<yarp::sig::PixelBgr> getImage();
+    bool isImageReady() { return imageReady;};
+};
+
+
 class vMappingModule : public yarp::os::RFModule {
 private :
+    
+    ImagePort imagePortIn;
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelBgr > > imagePortOut;
+    EventBottleManager vPort;
 
 public :
     
@@ -25,5 +79,7 @@ public :
     virtual double getPeriod();
     
 };
+
+
 
 #endif //ICUB_EVENT_DRIVEN_VMAPPING_H
--- src/processing/vMapping/src/vMapping.cpp
+++ src/processing/vMapping/src/vMapping.cpp
@@ -18,7 +18,7 @@ int main(int argc, char * argv[])
     yarp::os::ResourceFinder rf;
     rf.setVerbose( true );
     rf.setDefaultContext( "eventdriven" );
-    rf.setDefaultConfigFile( "vFramer.ini" );
+    rf.setDefaultConfigFile( "vMapping.ini" );
     rf.configure( argc, argv );
     
     vMappingModule mappingModule;
@@ -26,21 +26,148 @@ int main(int argc, char * argv[])
 }
 
 bool vMappingModule::updateModule() {
-    return true; //TODO
+   
+    if (imagePortIn.isImageReady()){
+        yarp::sig::ImageOf<yarp::sig::PixelBgr > &frame = imagePortOut.prepare();
+        frame = imagePortIn.getImage();
+        
+        ev::vQueue vQueue = vPort.getEvents();
+
+        for (auto it = vQueue.begin(); it != vQueue.end(); ++it){
+            auto v = ev::is_event<ev::AE >(*it);
+            unsigned int x = (304 - v->x);
+            unsigned int y = (240 - v->y);
+//            x *= 4.21;
+//            y *= 4.26;
+            frame(x,y) = yarp::sig::PixelBgr(255,0,0);
+        }
+          imagePortOut.write();
+    }
+    
+    return true;
 }
 
 bool vMappingModule::configure( yarp::os::ResourceFinder &rf ) {
-    return true;//TODO
+    std::string moduleName = rf.check("name",yarp::os::Value("/vMapping")).asString();
+    setName(moduleName.c_str());
+    
+    imagePortIn.open(getName("/img:i"));
+    vPort.open(getName("/vBottle:i"));
+    imagePortOut.open(getName("/img:o"));
+    vPort.start();
+    return true;
 }
 
 bool vMappingModule::interruptModule() {
-    return true;//TODO
+    imagePortOut.interrupt();
+    imagePortIn.interrupt();
+    vPort.interrupt();
+    return true;
 }
 
 bool vMappingModule::close() {
-    return true;//TODO
+    imagePortOut.close();
+    imagePortIn.close();
+    vPort.close();
+    return true;
 }
 
 double vMappingModule::getPeriod() {
-    return 0;//TODO
+    return 0;
+}
+
+/**************************ImagePort*********************************/
+
+yarp::sig::ImageOf<yarp::sig::PixelBgr> ImagePort::getImage(){
+    yarp::sig::ImageOf<yarp::sig::PixelBgr> outImg;
+    mutex.lock();
+    outImg = image;
+    imageReady = false;
+    mutex.unlock();
+    return outImg;
+};
+
+void ImagePort::onRead( yarp::sig::ImageOf<yarp::sig::PixelBgr> &inImg ){
+    mutex.lock();
+    image = inImg;
+    imageReady = true;
+    mutex.unlock();
+};
+
+/***********************EventBottleManager***********************/
+
+EventBottleManager::EventBottleManager() {
+    
+    //here we should initialise the module
+    vCount = 0;
+    latestStamp = 0;
+    isReading = false;
+}
+
+bool EventBottleManager::open(const std::string &name) {
+    //and open the input port
+    
+    this->useCallback();
+    
+    BufferedPort<ev::vBottle>::open(name);
+    return true;
+}
+
+void EventBottleManager::onRead(ev::vBottle &bot) {
+    if (!isReading)
+        return;
+    
+    //get new events
+    ev::vQueue newQueue = bot.get<ev::AE>();
+    if(newQueue.empty()){
+        return;
+    }
+    
+    mutex.wait();
+    //append new events to queue
+    vQueue.insert(vQueue.end(), newQueue.begin(), newQueue.end());
+    latestStamp = unwrapper(newQueue.back()->stamp);
+    vCount += newQueue.size();
+    mutex.post();
+}
+
+unsigned long int EventBottleManager::getTime() {
+    return latestStamp;
+    
+}
+
+unsigned long int EventBottleManager::popCount() {
+    mutex.wait();
+    unsigned long int r = vCount;
+    vCount = 0;
+    mutex.post();
+    return r;
+    
+}
+
+bool EventBottleManager::start() {
+    mutex.wait();
+    vQueue.clear();
+    isReading = true;
+    yRate = yarp::os::Time::now();
+    mutex.post();
+    return true;
+}
+
+bool EventBottleManager::stop() {
+    mutex.wait();
+    isReading = false;
+    yRate = yarp::os::Time::now() - yRate;
+    yRate = vCount / yRate;
+    vCount = 0;
+    mutex.post();
+    return true;
+}
+
+ev::vQueue EventBottleManager::getEvents() {
+    if (!&vQueue)
+        return ev::vQueue();
+    ev::vQueue outQueue = vQueue;
+    vQueue.clear();
+    return outQueue;
 }
