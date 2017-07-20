fb6549fd0f67ebecf69c10fa3038ccfe70e225d4
icub24 <user@icub24>
2017-07-20 18:31:13 +0200

    Homography mapping working!

--- src/processing/vMapping/src/vMapping.cpp
+++ src/processing/vMapping/src/vMapping.cpp
@@ -27,57 +27,7 @@ int main(int argc, char * argv[])
 }
 
 bool vMappingModule::updateModule() {
-    
-    if (imageCollector.isImageReady() && vImageCollector.isImageReady()){
-        yarp::sig::ImageOf<yarp::sig::PixelBgr> frame = imageCollector.getImage();
-        yarp::sig::ImageOf<yarp::sig::PixelBgr> vImg = vImageCollector.getImage();
-        
-        IplImage* imgL= (IplImage*) frame.getIplImage();
-        cv::Mat mat= cv::cvarrToMat(imgL);
-        cv::imshow("img",mat);
-        
-        IplImage* vImgL= (IplImage*) vImg.getIplImage();
-        cv::Mat vMat= cv::cvarrToMat(vImgL);
-        cv::imshow("vImg",vMat);
-        
-        cv::waitKey(1);
-        cv::Size boardSize (4 , 11);
-        std::vector <cv::Point2f> pointbufL;
-        std::vector <cv::Point2f> pointbufR;
-        
-        bool foundL = cv::findCirclesGrid(mat, boardSize, pointbufL, cv::CALIB_CB_ASYMMETRIC_GRID | cv::CALIB_CB_CLUSTERING);
-        bool foundR = cv::findCirclesGrid(vMat, boardSize, pointbufR, cv::CALIB_CB_ASYMMETRIC_GRID | cv::CALIB_CB_CLUSTERING);
-        
-        if (foundL) std::cout << "frame" << std::endl;
-        if (foundR) std::cout << "events" << std::endl;
-        if(foundL && foundR) {
-            std::cout << "found" << std::endl;
-            cvCvtColor(imgL,imgL,CV_RGB2BGR);
-            cvCvtColor(vImgL,vImgL, CV_RGB2BGR);
-//            cv::saveStereoImage(pathImg.c_str(),imgL,vImgL,count);
-
-//            imageListR.push_back(imr);
-//            imageListL.push_back(iml);
-//            imageListLR.push_back(iml);
-//            imageListLR.push_back(imr);
-            cv::Mat cL(pointbufL);
-            cv::Mat cR(pointbufR);
-            drawChessboardCorners(mat, boardSize, cL, foundL);
-            drawChessboardCorners(vMat, boardSize, cR, foundR);
-            cv::Mat h = cv::findHomography(pointbufL, pointbufR, CV_RANSAC);
-            for (int r = 0; r < h.rows; ++r) {
-                for (int c  = 0; c  < h.cols; ++c ) {
-                    std::cout << h.at(r,c) << " ";
-                }
-                std::cout << std::endl;
-            }
-            
-//            count++;
-        }
-        
-    }
-    
-    return true;
+   
     if (imageCollector.isImageReady()){
         yarp::sig::ImageOf<yarp::sig::PixelBgr > &frame = imagePortOut.prepare();
         frame = imageCollector.getImage();
@@ -98,7 +48,7 @@ bool vMappingModule::updateModule() {
 //            std::cout << "x bef = " << x << std::endl;
 //            std::cout << "y bef = " << y << std::endl;
             
-//            evCoord *= homography;
+            evCoord *= homography;
             
 //            std::cout << "evCoord[0] = " << evCoord[0] << std::endl;
 //            std::cout << "evCoord[1] = " << evCoord[1] << std::endl;
@@ -116,6 +66,52 @@ bool vMappingModule::updateModule() {
         imagePortOut.write();
     }
     
+    return true; 
+
+    if (imageCollector.isImageReady() && vImageCollector.isImageReady()){
+        yarp::sig::ImageOf<yarp::sig::PixelBgr> frame = imageCollector.getImage();
+        yarp::sig::ImageOf<yarp::sig::PixelBgr> vImg = vImageCollector.getImage();
+        
+        IplImage* imgL= (IplImage*) frame.getIplImage();
+        cv::Mat mat= cv::cvarrToMat(imgL);
+        cv::imshow("img",mat);
+        
+        IplImage* vImgL= (IplImage*) vImg.getIplImage();
+        cv::Mat vMat= cv::cvarrToMat(vImgL);
+        cv::imshow("vImg",vMat);
+        
+        cv::waitKey(1);
+        cv::Size boardSize (4 , 11);
+        std::vector <cv::Point2f> pointbufL;
+        std::vector <cv::Point2f> pointbufR;
+        
+        bool foundL = cv::findCirclesGrid(mat, boardSize, pointbufL, cv::CALIB_CB_ASYMMETRIC_GRID | cv::CALIB_CB_CLUSTERING);
+        bool foundR = cv::findCirclesGrid(vMat, boardSize, pointbufR, cv::CALIB_CB_ASYMMETRIC_GRID | cv::CALIB_CB_CLUSTERING);
+        
+        //if (foundL) std::cout << "frame" << std::endl;
+        //if (foundR) std::cout << "events" << std::endl;
+        if(foundL && foundR) {
+            //std::cout << "found" << std::endl;
+            cvCvtColor(imgL,imgL,CV_RGB2BGR);
+            cvCvtColor(vImgL,vImgL, CV_RGB2BGR);
+//            cv::saveStereoImage(pathImg.c_str(),imgL,vImgL,count);
+
+//            imageListR.push_back(imr);
+//            imageListL.push_back(iml);
+//            imageListLR.push_back(iml);
+//            imageListLR.push_back(imr);
+            cv::Mat cL(pointbufL);
+            cv::Mat cR(pointbufR);
+            drawChessboardCorners(mat, boardSize, cL, foundL);
+            drawChessboardCorners(vMat, boardSize, cR, foundR);
+            cv::Mat h = cv::findHomography(pointbufR, pointbufL, CV_RANSAC);
+            std::cout << h << std::endl << std::endl;
+            
+//            count++;
+        }
+        
+    }
+    
     return true;
 }
 
@@ -123,15 +119,16 @@ bool vMappingModule::configure( yarp::os::ResourceFinder &rf ) {
     std::string moduleName = rf.check("name",yarp::os::Value("/vMapping")).asString();
     setName(moduleName.c_str());
     homography.resize(3,3);
-    homography(0,0) = 5.61627318;
-    homography(0,1) = -9.53359703e-01;
-    homography(0,2) = -4.22708305e+02;
-    homography(1,0) = 1.46739935e-01;
-    homography(1,1) =  5.14009647;
-    homography(1,2) = -1.40622260e+02;
-    homography(2,0) = -2.84753637e-04;
-    homography(2,1) = -4.05376215e-04;
+    homography(0,0) = 1.553509692150836;
+    homography(0,1) = -0.03204146238532902;
+    homography(0,2) = -126.1716599747018;
+    homography(1,0) = 0.002140828524680886;
+    homography(1,1) =  1.481284340326434;
+    homography(1,2) = -8.950266164271969;
+    homography(2,0) = 0.0001706129403882854;
+    homography(2,1) = -0.0004468557077937145;
     homography(2,2) = 1;
+
     homography = homography.transposed();
 //    homography = yarp::sig::Matrix::eye();
     for (int r = 0; r < homography.rows(); ++r) {
