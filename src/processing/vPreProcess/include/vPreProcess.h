50b8c82f1221180ebde7a5b416f6f54e017763f1
arrenglover <arren.glover@gmail.com>
2017-07-27 16:16:32 +0200

    created vPreProcess

---
+++ src/processing/vPreProcess/include/vPreProcess.h
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author: Chiara Bartolozzi
+ * email:  chiara.bartolozzi@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+ */
+
+// \defgroup Modules Modules
+// \defgroup vPepper vPepper
+// \ingroup Modules
+// \brief removes salt-and-pepper noise from the event stream
+
+#ifndef __VPREPROCESS__
+#define __VPREPROCESS__
+
+#include <yarp/os/all.h>
+#include <iCub/eventdriven/all.h>
+#include <opencv/cv.h>
+using namespace::ev;
+
+class vPreProcess : public yarp::os::Thread
+{
+private:
+
+    //output port for the vBottle with the new events computed by the module
+    ev::queueAllocator inPort;
+    yarp::os::BufferedPort<ev::vBottle> outPort;
+
+    //parameters
+    std::string name;
+    ev::resolution res;
+
+    //pre-pre processing
+    bool precheck;
+    bool flipx;
+    bool flipy;
+
+    //filter class
+    bool pepper;
+    ev::vNoiseFilter thefilter;
+
+    //we store an openCV map to use as a look-up table for the undistortion
+    //given the camera parameters provided
+    bool undistort;
+    cv::Mat leftMap;
+    cv::Mat rightMap;
+    bool truncate;
+
+public:
+
+    vPreProcess();
+    ~vPreProcess();
+
+    void initBasic(std::string name, int height, int width, bool precheck,
+                   bool flipx, bool flipy, bool pepper, bool undistort);
+    void initPepper(int spatialSize, int temporalSize);
+    void initUndistortion(const yarp::os::Bottle &left,
+                          const yarp::os::Bottle &right, bool truncate);
+    void run();
+    void onStop();
+    bool threadInit();
+
+};
+
+class vPreProcessModule : public yarp::os::RFModule
+{
+    //the event bottle input and output handler
+    vPreProcess      eventManager;
+
+public:
+
+    //the virtual functions that need to be overloaded
+    virtual bool configure(yarp::os::ResourceFinder &rf);
+    virtual bool close();
+
+    virtual double getPeriod();
+    virtual bool updateModule();
+
+};
+
+
+#endif
+
