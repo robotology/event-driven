d9a386072d89ec3e9e25b04322350c61db161dae
arrenglover <arren.glover@iit.it>
2018-10-11 12:13:19 +0200

    added more CMAKE options to reduce buildtime

--- libraries/include/iCub/eventdriven/all.h
+++ libraries/include/iCub/eventdriven/all.h
@@ -1,10 +1,12 @@
 #include "iCub/eventdriven/vtsHelper.h"
 #include "iCub/eventdriven/vCodec.h"
-#include "iCub/eventdriven/vBottle.h"
+#include "iCub/eventdriven/vPort.h"
 #include "iCub/eventdriven/vFilters.h"
+
+#ifndef VLIB_LITE
+#include "iCub/eventdriven/vBottle.h"
 #include "iCub/eventdriven/vWindow_basic.h"
 #include "iCub/eventdriven/vWindow_adv.h"
 #include "iCub/eventdriven/vSurfaceHandlerTh.h"
 #include "iCub/eventdriven/vCollectSend.h"
-#include "iCub/eventdriven/vPort.h"
-
+#endif
