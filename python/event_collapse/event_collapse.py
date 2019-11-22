4a7ab5aa4e545a363993af0730a8b43422fce137
Arren Glover <arrenglover@users.noreply.github.com>
2019-11-22 09:12:13 +0100

    event_collapse add -r replace flag (#52)

--- python/event_collapse/event_collapse.py
+++ python/event_collapse/event_collapse.py
@@ -43,15 +43,25 @@ def collapse(filename, min_bot_duration=2e-3):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Collapse events in bottles of duration at least min_bot_duration milliseconds')
     parser.add_argument('in_file', type=str, help='Input log file')
-    parser.add_argument('--min_bot_duration', '-m', default=2, type=int, nargs=1, help='Minimum duration of bottle into which events are collapsed')
+    parser.add_argument('--min_bot_duration', '-m', default=2.0, type=float, help='Minimum duration of bottle into which events are collapsed')
     parser.add_argument('--out_file', type=str, help='Output log file')
+    parser.add_argument('--replace', '-r', action='store_true', help='Copy output in place of original')
 
     args = parser.parse_args()
 
     if args.out_file is None:
-        args.out_file = os.path.join(os.path.dirname(args.in_file), 'data_collapsed.log')
+        if args.replace:
+            args.out_file = args.in_file
+            temp_in_file = os.path.join(os.path.dirname(args.in_file), 'data.orig.log')
+            if os.path.exists(temp_in_file):
+                print("Cannot replace file as it already exists: {}".format(temp_in_file))
+                exit(-1)
+            os.rename(args.in_file, temp_in_file)
+            args.in_file = temp_in_file
+        else:
+            args.out_file = os.path.join(os.path.dirname(args.in_file), 'data_collapsed.log')
 
-    text_out = '\n'.join([line for line in collapse(args.in_file, args.min_bot_duration * 1e-3)])
+    text_out = '\n'.join([line for line in collapse(args.in_file, min_bot_duration=(args.min_bot_duration * 1e-3))])
     with open(args.out_file, 'w') as of:
         of.write(text_out)
     print("Written to {}".format(args.out_file))
