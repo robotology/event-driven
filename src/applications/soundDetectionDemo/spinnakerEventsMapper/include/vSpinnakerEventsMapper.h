d9b57b82aab7ec2855eefa63a9ef2b075da8aa17
dgutierrezATC <dgutierrez@atc.us.es>
2020-11-19 17:01:46 +0100

    Created SpinnakerEventsMapper module

---
+++ src/applications/soundDetectionDemo/spinnakerEventsMapper/include/vSpinnakerEventsMapper.h
@@ -0,0 +1,6 @@
+//
+// Created by Daniel Gutierrez-Galan
+// dgutierrez@atc.us.es
+// University of Seville
+// 19/Nov/2020
+//
---
+++ src/applications/soundDetectionDemo/spinnakerEventsMapper/src/vSpinnakerEventsMapper.cpp
@@ -0,0 +1,156 @@
+//
+// Created by Daniel Gutierrez-Galan
+// dgutierrez@atc.us.es
+// University of Seville
+// 19/Nov/2020
+//
+
+#include <yarp/os/all.h>
+#include <event-driven/all.h>
+using namespace ev;
+using namespace yarp::os;
+
+class vSpinnakerEventsMapper : public RFModule, public Thread {
+
+private:
+
+    vReadPort< vector<AE> > input_port;
+    vWritePort output_port;
+
+    bool is_debug_flag;
+    int example_parameter;
+
+public:
+
+    vSpinnakerEventsMapper() {}
+
+    virtual bool configure(yarp::os::ResourceFinder& rf)
+    {
+        yInfo() << "Configuring the module...";
+
+        //set the module name used to name ports
+        setName((rf.check("name", Value("/vSpinnakerEventsMapper")).asString()).c_str());
+
+        //open io ports
+        yInfo() << "Opening input port...";
+        if(!input_port.open(getName() + "/AE:i")) {
+            yError() << "Could not open input port";
+            return false;
+        }
+        yInfo() << "Opening output port...";
+        output_port.setWriteType(AE::tag);
+        if(!output_port.open(getName() + "/AE:o")) {
+            yError() << "Could not open input port";
+            return false;
+        }
+
+        //read flags and parameters
+        is_debug_flag = rf.check("is_debug_flag") &&
+                rf.check("is_debug_flag", Value(true)).asBool();
+        yInfo() << "Flag is_debug_flat is: " << is_debug_flag;
+
+        int default_example_parameter = 32;
+        example_parameter = rf.check("example_parameter", 
+                                    Value(default_example_parameter)).asInt();
+        yInfo() << "Setting example_parameter parameter to: " << example_parameter;
+
+        //do any other set-up required here
+
+        //start the asynchronous and synchronous threads
+        yInfo() << "Starting the thread...";
+        return Thread::start();
+    }
+
+    virtual double getPeriod()
+    {
+        return 1.0; //period of synchrnous thread
+    }
+
+    bool interruptModule()
+    {
+        //if the module is asked to stop ask the asynchrnous thread to stop
+        yInfo() << "Interrupting the module: stopping thread...";
+        return Thread::stop();
+    }
+
+    void onStop()
+    {
+        //when the asynchrnous thread is asked to stop, close ports and do
+        //other clean up
+        yInfo() << "Stopping the module...";
+        yInfo() << "Closing input port...";
+        input_port.close();
+        yInfo() << "Closing output port...";
+        output_port.close();
+        yInfo() << "Module has been closed!";
+    }
+
+    //synchronous thread
+    virtual bool updateModule()
+    {
+
+        //add any synchronous operations here, visualisation, debug out prints
+
+        // Try to print here the spikegram, the histogram, the mean activity, the heatmap...
+
+
+        return Thread::isRunning();
+    }
+
+    //asynchronous thread run forever
+    void run()
+    {
+        Stamp yarpstamp;
+        deque<AE> out_queue;
+        AE out_event;
+        int address = 0;
+
+        while(true) {
+
+            const vector<AE> * q = input_port.read(yarpstamp);
+            if(!q || Thread::isStopping()) return;
+
+            //do asynchronous processing here
+            for(auto &qi : *q) {
+
+                //here you could try modifying the data of the event before
+                //pushing to the output q
+
+                // Get the real AER address
+
+                if (is_debug_flag == true) {
+                    yDebug() << "Event received and decoded address: " << address;
+                }
+                
+            }
+
+            //after processing the packet output the results
+            //(only if there is something to output
+            if(out_queue.size()) {
+                output_port.write(out_queue, yarpstamp);
+                out_queue.clear();
+            }
+        }
+    }
+};
+
+int main(int argc, char * argv[])
+{
+    /* initialize yarp network */
+    yarp::os::Network yarp;
+    if(!yarp.checkNetwork(2)) {
+        std::cout << "Could not connect to YARP" << std::endl;
+        return false;
+    }
+
+    /* prepare and configure the resource finder */
+    yarp::os::ResourceFinder rf;
+    rf.setVerbose( false );
+    rf.setDefaultContext( "eventdriven" );
+    rf.setDefaultConfigFile( "vSpinnakerEventsMapper.ini" );
+    rf.configure( argc, argv );
+
+    /* create the module */
+    vSpinnakerEventsMapper instance;
+    return instance.runModule(rf);
+}
