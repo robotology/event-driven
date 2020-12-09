1b2eef76df11f146ae12bda51e1a26cc34eaa094
dgutierrezATC <dgutierrez@atc.us.es>
2020-12-09 13:07:37 +0100

    Improved code comments and added real-time histogram for NAS

--- src/applications/soundDetectionDemo/cochleaEventsMapper/src/vCochleaEventsMapper.cpp
+++ src/applications/soundDetectionDemo/cochleaEventsMapper/src/vCochleaEventsMapper.cpp
@@ -25,19 +25,28 @@ private:
     // If activated, the module shows info about the classification in the terminal
     bool is_debug_flag;
 
-    // Dummy parameter
-    int example_parameter;
+    // Period
+    double update_period;
 
     // Matrix of coincidence counters from MSO
     // ----------------------------------------------------------------------------
     // WARNING!! This matrix should have the same dimensions as the MSO model
     // used in the VHDL model
     // ----------------------------------------------------------------------------
-    int mso_histogram[4][16];
-    int mso_start_freq_channel = 13;
+    int mso_histogram[CochleaEvent::mso_num_freq_channels][CochleaEvent::mso_num_neurons_per_channel];
 
     // Visualizer output image
-    cv::Mat image_mso_heatmap = cv::Mat::zeros(cv::Size(320,240), CV_8UC3);
+    int image_mso_heatmap_width = 320;
+    int image_mso_heatmap_height = 240;
+    cv::Mat image_mso_heatmap = cv::Mat::zeros(cv::Size(image_mso_heatmap_width,image_mso_heatmap_height), CV_8UC3);
+
+    // Array of freq. channels activity
+    int nas_histogram[CochleaEvent::nas_addrresses_offset];
+
+    // Visualizer output image NAS 
+    int image_nas_histogram_width = 320;
+    int image_nas_histogram_height = 240;
+    cv::Mat image_nas_histogram = cv::Mat::zeros(cv::Size(image_nas_histogram_width, image_nas_histogram_height), CV_8UC3);
 
 public:
 
@@ -71,20 +80,28 @@ public:
                 rf.check("is_debug_flag", Value(true)).asBool();
         yInfo() << "Flag is_debug_flat is: " << is_debug_flag;
 
-        // Dummy parameter: 32 by default
-        int default_example_parameter = 32;
-        example_parameter = rf.check("example_parameter", 
-                                    Value(default_example_parameter)).asInt();
-        yInfo() << "Setting example_parameter parameter to: " << example_parameter;
+        // Period value: 1 sec by default
+        double default_update_period = 1.0;
+        update_period = rf.check("update_period",
+                                    Value(default_update_period)).asDouble();
+        yInfo() << "Setting update_period parameter to: " << update_period;
 
-        // Initialize the mso coincidence counters to zero
+        // Initialize the MSO coincidence counters to zero
         // ----------------------------------------------------------------------------
         // WARNING!! This values should match with the ones used before
         // ----------------------------------------------------------------------------
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 16; j++) {
+        for(int i = 0; i < CochleaEvent::mso_num_freq_channels; i++) {
+            for(int j = 0; j < CochleaEvent::mso_num_neurons_per_channel; j++) {
                 mso_histogram[i][j] = 0;
             }
+        }
+
+        // Initialize the NAS histogram
+        // ----------------------------------------------------------------------------
+        // WARNING!! This values should match with the ones used before
+        // ----------------------------------------------------------------------------
+        for(int i = 0; i < CochleaEvent::nas_addrresses_offset; i++) {
+            nas_histogram[i] = 0;
         }
 
         // Do any other set-up required here
@@ -97,7 +114,7 @@ public:
     virtual double getPeriod()
     {
         // Period of synchrnous thread (in seconds)
-        return 1.0;
+        return update_period;
     }
 
     bool interruptModule()
@@ -124,7 +141,12 @@ public:
     {
         // Add any synchronous operations here, visualisation, debug out prints
 
-        // Do any other set-up required here
+        // ----------------------------------------------------------------------------
+        //                          HEATMAP DRAW SECTION
+        // ----------------------------------------------------------------------------
+        // Define the image settings
+        float rectangle_width = ((float)(image_mso_heatmap_width)) / CochleaEvent::mso_num_neurons_per_channel;
+        float rectangle_height = ((float)(image_mso_heatmap_height)) / CochleaEvent::mso_num_freq_channels;
 
         // Calculate max and min val (for normalization) and the position of the 
         // max value for using another color
@@ -136,8 +158,8 @@ public:
         int max_position_i = 0;
         int max_position_j = 0;
 
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 16; j++) {
+        for(int i = 0; i < CochleaEvent::mso_num_freq_channels; i++) {
+            for(int j = 0; j < CochleaEvent::mso_num_neurons_per_channel; j++) {
                 float val = (float)(mso_histogram[i][j]);
                 if(val > max){
                     max = val;
@@ -153,18 +175,18 @@ public:
         // ----------------------------------------------------------------------------
         //                          COLOR GRADIENT ESTIMATION
         // Calculate the color based on the normalized value
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 16; j++) {
+        for(int i = 0; i < CochleaEvent::mso_num_freq_channels; i++) {
+            for(int j = 0; j < CochleaEvent::mso_num_neurons_per_channel; j++) {
                 
                 float pixelValue = (float)(mso_histogram[i][j]);
                 float value = ((pixelValue - min) / (max - min));
 
-                int aR = 0;   int aG = 0; int aB=255;     // RGB for our 1st color (blue in this case).
-                int bR = 255; int bG = 0; int bB=0;       // RGB for our 2nd color (red in this case).
+                int aR = 0;   int aG = 0; int aB = 255;     // RGB for our 1st color (blue in this case).
+                int bR = 255; int bG = 0; int bB = 0;       // RGB for our 2nd color (red in this case).
 
-                int r = (float)(bR - aR) * value + aR;    // Evaluated as -255*value + 255.
-                int g = (float)(bG - aG) * value + aG;    // Evaluates as 0.
-                int b = (float)(bB - aB) * value + aB;    // Evaluates as 255*value + 0.
+                int r = (float)(bR - aR) * value + aR;      // Evaluated as -255*value + 255.
+                int g = (float)(bG - aG) * value + aG;      // Evaluates as 0.
+                int b = (float)(bB - aB) * value + aB;      // Evaluates as 255*value + 0.
 
                 // If the current rectangle is the winner, use the color green
                 if((i == max_position_i) && (j == max_position_j)){
@@ -175,7 +197,7 @@ public:
                 // ----------------------------------------------------------------------------
                 //                          RECTANGLE DRAW
                 // Draw the rectangle
-                cv::Rect rect(j*20, i*60, 20, 60);
+                cv::Rect rect(j*rectangle_width, i*rectangle_height, rectangle_width, rectangle_height);
                 cv::rectangle(image_mso_heatmap, rect, cv::Vec3b(b, g, r), -1);
             }
         }
@@ -185,11 +207,71 @@ public:
         cv::waitKey(10);
 
         // Clean the mso matrix values before the next update
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 16; j++) {
+        for(int i = 0; i < CochleaEvent::mso_num_freq_channels; i++) {
+            for(int j = 0; j < CochleaEvent::mso_num_neurons_per_channel; j++) {
                 mso_histogram[i][j] = 0;
             }
         }
+
+        // ----------------------------------------------------------------------------
+        //                          HISTOGRAM DRAW SECTION
+        // ----------------------------------------------------------------------------
+
+        // ----------------------------------------------------------------------------
+        //                          CLEAR IMAGE
+        // Remove all the previous plot
+        image_nas_histogram = cv::Mat::zeros(cv::Size(image_nas_histogram_width, image_nas_histogram_height), CV_8UC3);
+
+        // ----------------------------------------------------------------------------
+        //                          IMAGE SETTING
+        // Define the image settings
+        float bar_width = ((float)(image_nas_histogram_width)) / CochleaEvent::nas_addrresses_offset;
+        float bar_height = 0.0f;
+
+        // Horizontal and vertical margins: set to 3% of the image height
+        float margin = image_nas_histogram.cols * 0.03;
+
+        // Calculate max and min val (for normalization) 
+        float max = 0.0f;
+        float min = FLT_MAX;
+        int max_position_i = 0;
+
+        for(int i = 0; i < CochleaEvent::nas_addrresses_offset; i++) {
+                float val = (float)(nas_histogram[i]);
+                if(val > max){
+                    max = val;
+                    max_position_i = i;
+                }
+                if(val < min){
+                    min = val;
+                }
+            }
+        }
+
+        // ----------------------------------------------------------------------------
+        //                          HISTOGRAM DRAW
+        for(int i = 0; i < CochleaEvent::nas_addrresses_offset; i++) {
+            // First, get the normalized value of the neuron activity
+            float normalized_histogram_value = (nas_histogram[i] - min) * 100.0 / (max - min);
+            
+            // Define left top point
+            float rec_x_top_left = i * bar_width;
+            // The y value is the bottom reference value minus the normalized value (the width of the histogram bar)
+            float rec_y_top_left = 5 * (image_nas_histogram.rows/6) - normalized_histogram_value;
+            cv::Point p1(rec_x_top_left, rec_y_top_left);
+
+            // Define right bottom point
+            // X value is the same as left top point plus the diameter of the circle (radius * 2)
+            float rec_x_bottom_right = rec_x_top_left + bar_width;
+            float rec_y_bottom_right = 5 * (image_nas_histogram.rows/6);
+            cv::Point p2(rec_x_bottom_right, rec_y_bottom_right);
+
+            // Create the rectangle
+            cv::Vec3b c = cv::Vec3b(255, 255, 255);
+            cv::rectangle(image_nas_histogram, p1, p2, c);
+        }
+
+        // Do any other set-up required here
 
         return Thread::isRunning();
     }
@@ -208,13 +290,6 @@ public:
 
         // Raw address received from the input port
         int address = 0;
-
-        // Initialize the first time the mso matrix values
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 16; j++) {
-                mso_histogram[i][j] = 0;
-            }
-        }
 
         // Forever...
         while(true) {
@@ -255,7 +330,7 @@ public:
                         // Get the freq channel id
                         int ch = qi.freq_chnn;
                         // Substract the MSO start channel to start the IDs in zero
-                        ch = ch - mso_start_freq_channel;
+                        ch = ch - CochleaEvent::mso_start_freq_channel;
                         // Get the neuron ID
                         int ne = qi.neuron_id;
                         // Increment the MSO matrix values in one
