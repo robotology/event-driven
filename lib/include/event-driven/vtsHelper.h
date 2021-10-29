a04890ff114def5231ab47cd812b0ade48a25a50
arrenglover <arren.glover@iit.it>
2021-10-29 15:46:52 +0200

    added AHRS code

--- lib/include/event-driven/vtsHelper.h
+++ lib/include/event-driven/vtsHelper.h
@@ -113,9 +113,8 @@ class imuAdvHelper
 {
 private:
 
-    static constexpr double sampleFreq = 512.0;
-    static constexpr double twoKpDef = 1.0;
-    static constexpr double twoKiDef = 0.0;
+    static constexpr double beta = 0.1;
+    static constexpr double g = 9.80665;
     
     enum
     {
@@ -125,49 +124,139 @@ private:
         MAG_Y = 7, MAG_X = 8, MAG_Z = 9
     };
 
-    double heading_q[4];
-    double imu_readings[10];
-    std::vector<double> accels;
-    std::array<double, 6> vels;
+    std::array<double, 10> imu_readings{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    std::array<double, 6> accels{0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    std::array<double, 6> vels{0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    std::array<double, 4> heading{0.0, 0.0, 0.0, 0.0};
 
-    void updateHeading() 
+    void updateAHRS(double gx, double gy, double gz, double &ax, double &ay, double &az, double dt, double beta) {
+
+        double &q0 = heading[0];
+        double &q1 = heading[1];
+        double &q2 = heading[2];
+        double &q3 = heading[3];
+        float recipNorm;
+        float s0, s1, s2, s3;
+        float qDot1, qDot2, qDot3, qDot4;
+        float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2, _8q1, _8q2, q0q0, q1q1, q2q2, q3q3;
+        double axc = ax, ayc = ay, azc = az;
+
+        // Rate of change of quaternion from gyroscope
+        qDot1 = 0.5f * (-q1 * gx - q2 * gy - q3 * gz);
+        qDot2 = 0.5f * (q0 * gx + q2 * gz - q3 * gy);
+        qDot3 = 0.5f * (q0 * gy - q1 * gz + q3 * gx);
+        qDot4 = 0.5f * (q0 * gz + q1 * gy - q2 * gx);
+
+        // Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
+        if (!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
+            // Normalise accelerometer measurement
+            recipNorm = invSqrt(ax * ax + ay * ay + az * az);
+            ax *= recipNorm;
+            ay *= recipNorm;
+            az *= recipNorm;
+
+            // Auxiliary variables to avoid repeated arithmetic
+            _2q0 = 2.0f * q0;
+            _2q1 = 2.0f * q1;
+            _2q2 = 2.0f * q2;
+            _2q3 = 2.0f * q3;
+            _4q0 = 4.0f * q0;
+            _4q1 = 4.0f * q1;
+            _4q2 = 4.0f * q2;
+            _8q1 = 8.0f * q1;
+            _8q2 = 8.0f * q2;
+            q0q0 = q0 * q0;
+            q1q1 = q1 * q1;
+            q2q2 = q2 * q2;
+            q3q3 = q3 * q3;
+
+            // Gradient decent algorithm corrective step
+            s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
+            s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * q1 - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
+            s2 = 4.0f * q0q0 * q2 + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
+            s3 = 4.0f * q1q1 * q3 - _2q1 * ax + 4.0f * q2q2 * q3 - _2q2 * ay;
+            recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3);  // normalise step magnitude
+            s0 *= recipNorm;
+            s1 *= recipNorm;
+            s2 *= recipNorm;
+            s3 *= recipNorm;
+
+            // Apply feedback step
+            qDot1 -= beta * s0;
+            qDot2 -= beta * s1;
+            qDot3 -= beta * s2;
+            qDot4 -= beta * s3;
+        }
+
+        // Integrate rate of change of quaternion to yield quaternion
+        q0 += qDot1 * dt;
+        q1 += qDot2 * dt;
+        q2 += qDot3 * dt;
+        q3 += qDot4 * dt;
+
+        // Normalise quaternion
+        recipNorm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
+        q0 *= recipNorm;
+        q1 *= recipNorm;
+        q2 *= recipNorm;
+        q3 *= recipNorm;
+
+        //compensate the accelerometer
+        ax = axc - g * 2 * (q1 * q3 - q0 * q2);
+        ay = ayc - g * 2 * (q0 * q1 + q2 * q3);
+        az = azc - g * (q0 * q0 - q1 * q1 - q2 * q2 + q3 * q3);
+    }
+
+    float invSqrt(float x) {
+        float halfx = 0.5f * x;
+        float y = x;
+        long i = *(long*)&y;
+        i = 0x5f3759df - (i >> 1);
+        y = *(float*)&i;
+        y = y * (1.5f - (halfx * y * y));
+        return y;
+    }
+
+    void updateHeading(double dt) 
     {
         //use actual calibration values to convert this
-        accels[0] = imu_readings[ACC_X] * 9.80665/16384.0;
-        accels[1] = -imu_readings[ACC_Y] * 9.80665/16384.0;
-        accels[2] = imu_readings[ACC_Z] * 9.80665/16384.0;
+        accels[0] = imu_readings[ACC_X] * g/16384.0;
+        accels[1] = -imu_readings[ACC_Y] * g/16384.0;
+        accels[2] = imu_readings[ACC_Z] * g/16384.0;
 
         vels[3] = imu_readings[GYR_X] * (250.0 * M_PI / (2.0 * 180.0 * 16384.0));
         vels[4] = -imu_readings[GYR_Y] * (250.0 * M_PI / (2.0 * 180.0 * 16384.0));
         vels[5] = imu_readings[GYR_Z] * (250.0 * M_PI / (2.0 * 180.0 * 16384.0));
 
         //update the AHRS
+        updateAHRS(vels[3], vels[4], vels[5], accels[0], accels[1], accels[2], dt, beta);
 
         //extract the best values for accel and vel
+        vels[0] += accels[0] * dt * 0.5;
+        vels[1] += accels[1] * dt * 0.5;
+        vels[2] += accels[2] * dt * 0.5;
 
     }
 
 
 public:
 
-    imuAdvHelper() 
-    {
-        accels.resize(6, 0.0);
-        //vels.resize(6, 0.0);
-    }
-
     bool loadIMUCalibrationFiles() {return true;}
     template <typename T> void addIMUPacket(const std::vector<T> &packet) 
     {
+        static int tic = packet.front().stamp;
         for(auto &v: packet) {
             imu_readings[v.sensor] = v.value;
-            if(v.sensor == 9)
-                updateHeading();
+            if(v.sensor == 9) {
+                updateHeading(vtsHelper::deltaS(v.stamp, tic));
+                tic = v.stamp;
+            }
         }
     }
 
-    const std::vector<double>& extractAcceleration() { return accels;}
+    const std::array<double, 6>& extractAcceleration() { return accels;}
     const std::array<double, 6>& extractVelocity() {return vels;}
+    const std::array<double, 4>& extractHeading() {return heading;}
 };
 
 class imuHelper {
