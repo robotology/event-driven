04fa2aa0948b6e51390b6908039676758beaa08e
arrenglover <arren.glover@iit.it>
2022-04-22 17:03:59 +0200

    added surfaces

---
+++ ev2/include/event-driven/algs.h
@@ -0,0 +1,1 @@
+#include "algs/surface.h"
---
+++ ev2/include/event-driven/algs/surface.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+
+namespace ev {
+
+class surface 
+{
+protected:
+    int kernel_size{0};
+    int half_kernel{0};
+    cv::Rect roi_full, roi_raw, roi_valid;
+    double parameter{0};
+    double time_now{0};
+
+    cv::Mat surf;
+    cv::Mat region;
+    cv::Mat sae;
+
+    bool setRoiAndRegion(int x, int y);
+
+public:
+   
+    const cv::Mat& getSurface();
+    virtual void init(int width, int height, int kernel_size, double parameter = 0.0);
+    virtual bool update(int x, int y, double ts, int p) = 0;
+    void temporalDecay(double ts);
+    void spatialDecay(int k);
+};
+
+class EROS : public surface
+{
+    bool update(int x, int y, double t = 0, int p = 0) override;
+};
+
+class TOS : public surface
+{
+    bool update(int x, int y, double t = 0, int p = 0) override;
+};
+
+class SITS : public surface
+{
+    bool update(int x, int y, double t = 0, int p = 0) override;
+};
+
+class PIM : public surface
+{
+    void init(int width, int height, int kernel_size, double parameter = 0.0) override;
+    bool update(int x, int y, double t = 0, int p = 0) override;
+};
+
+}
