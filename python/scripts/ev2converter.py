e3dd1254a6524fdb1b24183b9e5577ea894b0697
arrenglover <arren.glover@iit.it>
2023-02-21 10:45:20 +0100

    ev2converter 4ms bottles

--- python/scripts/ev2converter.py
+++ python/scripts/ev2converter.py
@@ -5,7 +5,7 @@ import os, shutil
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--in_path',  dest='in_path', type=str, required=True, help='Path to input file to be converted')
-parser.add_argument('--out_path', dest='out_path', type=str, nargs=1, help='Path to save converted file to')
+parser.add_argument('--out_path', dest='out_path', type=str, help='Path to save converted file to')
 args = parser.parse_args()
 
 if args.out_path is None:
@@ -15,13 +15,12 @@ if args.out_path is None:
         args.out_path = os.path.join(os.path.dirname(args.in_path), 'ev2')
 
 if os.path.exists(args.out_path):
-    if input(
-            f'Directory {args.out_path} already exists. Are you sure you want to replace all of its content? (y/n)\n') == 'y':
+    if input(f"Directory {args.out_path} already exists. Are you sure you want to replace all of its content? (y/n)") == 'y':
         shutil.rmtree(args.out_path)
     else:
         print('Exiting')
         exit(0)
 
 data = importIitYarp(filePathOrName=args.in_path)
-exportIitYarp(data, exportFilePath=args.out_path, exportAsEv2=True, exportTimestamps=False)
+exportIitYarp(data, exportFilePath=args.out_path, exportAsEv2=True, exportTimestamps=False, viewerApp=False, minTimeStepPerBottle=0.004)
 
