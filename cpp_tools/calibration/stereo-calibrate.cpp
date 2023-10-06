bcb65d1ca7f0b382f68c6845524f9e6ca981fa19
Arren Glover <arren.glover@iit.it>
2023-10-06 12:28:25 +0200

    stereo calib - reading and detecting 2 images

--- cpp_tools/calibration/stereo-calibrate.cpp
+++ cpp_tools/calibration/stereo-calibrate.cpp
@@ -31,7 +31,7 @@ private:
     ev::window<ev::AE> cam2;
 
     //provided parameters
-    cv::Size img_size, board_size;
+    cv::Size img_size_1, img_size_2, board_size;
     double edge_length;
 
     //calculated parameters
@@ -59,6 +59,10 @@ public:
             yInfo() << "--fout <str>\t: full path to output file";
             yInfo() << "--ch <int> --cw <int>\t: checkerboard corners height/width";
             yInfo() << "--cs <double>\t: checker square edge length in metres";
+            yInfo() << "--cam1cal  <string>\t: path to camera 1 parameter file";
+            yInfo() << "--cam2cal <string>\t: path to camera 2 parameter file";
+            yInfo() << "--cam1 <string>\t: port name of camera 1";
+            yInfo() << "--cam2 <string>\t: port name of camera 2";
             return false;
         }
 
@@ -90,14 +94,33 @@ public:
         board_size = cv::Size(rf.check("cw", Value(8)).asInt32(), rf.check("ch", Value(6)).asInt32());
 
         //get the intrinsic parameters
-        Bottle cam1_params = rf.findGroup("CAM1_CALIBRATION");
-        Bottle cam2_params = rf.findGroup("CAM2_CALIBRATION");
-        if(left_params.isNull() || right_params.isNull()) {
-            yError() << "Please supply intrinsic calibration file with [CAM1_CALIBRATION] "
-                        "and [CAM2_CALIBRATION] using the '--from <string>' argument";
+        ResourceFinder calibfinder;
+
+        if(!rf.check("cam1cal")) {
+            yError() << "please supply left camera parameters using --cam1cal <path>";
             return false;
         }
+        calibfinder.setDefault("from", rf.find("cam1cal").asString());
+        calibfinder.configure(0, 0);
+        yarp::os::Bottle params_1 = calibfinder.findGroup("CAMERA_CALIBRATION");
+        if(params_1.isNull()) {
+            yError() << "Could not find [CAMERA_CALIBRATION] in camera 1 file";
+            return false;
+        }
+        img_size_1 = {params_1.find("w").asInt32(), params_1.find("h").asInt32()};
 
+        if(!rf.check("cam2cal")) {
+            yError() << "please supply left camera parameters using --cam2cal <path>";
+            return false;
+        }
+        calibfinder.setDefault("from", rf.find("cam2cal").asString());
+        calibfinder.configure(0, 0);
+        yarp::os::Bottle params_2 = calibfinder.findGroup("CAMERA_CALIBRATION");
+        if(params_2.isNull()) {
+            yError() << "Could not find [CAMERA_CALIBRATION] in camera 2 file";
+            return false;
+        }
+        img_size_2 = {params_2.find("w").asInt32(), params_2.find("h").asInt32()};
 
         yInfo() << "STEREO EVENT-CAMERA CALIBRATION";
         yInfo() << "saving extrinsic calibration:" << fout;
@@ -116,8 +139,8 @@ public:
             return false;
         }
 
-        Network::connect("/atis3/cam1/AE:o", getName("/cam1/AE:i"), "fast_tcp");
-        Network::connect("/atis3/cam2/AE:o", getName("/cam2/AE:i"), "fast_tcp");
+        Network::connect(rf.check("cam1", Value("/atis4/cam1/AE:o")).asString(), getName("/cam1/AE:i"), "fast_tcp");
+        Network::connect(rf.check("cam2", Value("/atis4/cam2/AE:o")).asString(), getName("/cam2/AE:i"), "fast_tcp");
         
         return true;
     }
@@ -140,47 +163,63 @@ public:
     //synchronous thread
     bool updateModule() override
     {
-        static cv::Mat black_img = cv::Mat(img_size, CV_8UC3);
-        static cv::Mat detected_img = cv::Mat(img_size, CV_8UC3, black);
+        static cv::Mat black_img_1 = cv::Mat(img_size_1, CV_8UC3);
+        static cv::Mat black_img_2 = cv::Mat(img_size_2, CV_8UC3);
+        static cv::Mat detected_img_1 = cv::Mat(img_size_1, CV_8UC3, black);
+        static cv::Mat detected_img_2 = cv::Mat(img_size_2, CV_8UC3, black);
         static std::vector<int> bci = {0, 
                                        board_size.width-1, 
                                        board_size.area()-board_size.width, 
                                        board_size.area()-1};
-        black_img = ev::black;
-        ev::info stats = input.readSlidingWinT(0.033, false);
-        for (auto& v : input)
-            black_img.at<cv::Vec3b>(v.y, v.x) = white;
 
-        std::vector<cv::Point2f> corners;
-        bool found = cv::findChessboardCorners(black_img, board_size, corners);
-        cv::drawChessboardCorners(black_img, board_size, corners, found);
+        ev::info stats_1 = cam1.readSlidingWinT(0.033, false);
+        ev::info stats_2 = cam2.readSlidingWinT(0.033, false);
+        
+        black_img_1 = ev::black;
+        black_img_2 = ev::black;
+        for (auto& v : cam1)
+            black_img_1.at<cv::Vec3b>(v.y, v.x) = white*0.5;
+        for (auto& v : cam2)
+            black_img_2.at<cv::Vec3b>(v.y, v.x) = white*0.5;
+
+        std::vector<cv::Point2f> corners_1, corners_2;
+        bool found_1 = cv::findChessboardCorners(black_img_1, board_size, corners_1);
+        bool found_2 = cv::findChessboardCorners(black_img_2, board_size, corners_2);
+        cv::drawChessboardCorners(black_img_1, board_size, corners_1, found_1);
+        cv::drawChessboardCorners(black_img_2, board_size, corners_2, found_2);
         
 
-        bool calibrated = !map1.empty() && !map2.empty();
-        if(found && !calibrated) {
-            image_points.push_back(corners);
-            cv::line(detected_img, corners[bci[0]], corners[bci[1]], violet);
-            cv::line(detected_img, corners[bci[0]], corners[bci[2]], violet);
-            cv::line(detected_img, corners[bci[3]], corners[bci[1]], violet);
-            cv::line(detected_img, corners[bci[3]], corners[bci[2]], violet);
+        // bool calibrated = !map1.empty() && !map2.empty();
+        if(found_1 && found_2) {
+            //image_points.push_back(corners);
+            cv::line(detected_img_1, corners_1[bci[0]], corners_1[bci[1]], violet);
+            cv::line(detected_img_1, corners_1[bci[0]], corners_1[bci[2]], violet);
+            cv::line(detected_img_1, corners_1[bci[3]], corners_1[bci[1]], violet);
+            cv::line(detected_img_1, corners_1[bci[3]], corners_1[bci[2]], violet);
+
+            cv::line(detected_img_2, corners_2[bci[0]], corners_2[bci[1]], violet);
+            cv::line(detected_img_2, corners_2[bci[0]], corners_2[bci[2]], violet);
+            cv::line(detected_img_2, corners_2[bci[3]], corners_2[bci[1]], violet);
+            cv::line(detected_img_2, corners_2[bci[3]], corners_2[bci[2]], violet);
         }
 
-        black_img *= 0.5;
         //blue green red
-        if(calibrated) {
-            remap(black_img, black_img, map1, map2, cv::INTER_LINEAR);
-            cv::rectangle(black_img, cv::Rect(0, 0, img_size.width, img_size.height), green*0.5, 10);
-            cv::putText(black_img, "ESC to finish", cv::Point(img_size.width*0.05, img_size.height*0.95), cv::FONT_HERSHEY_PLAIN, 1.0, white);
-        } else {
-            black_img += detected_img;
-            cv::rectangle(black_img, cv::Rect(0, 0, img_size.width, img_size.height), red*0.8, 10);
-            cv::putText(black_img, "Collecting images... press SPACE to perform calibration", cv::Point(img_size.width*0.05, img_size.height*0.95), cv::FONT_HERSHEY_PLAIN, 1.0, white);
-            cv::putText(black_img, board_info, cv::Point(img_size.width*0.05, img_size.height*0.05), cv::FONT_HERSHEY_PLAIN, 1.0, white);
-            str_maker.str("");
-            str_maker << image_points.size();
-            cv::putText(black_img, str_maker.str(), cv::Point(img_size.width*0.95, img_size.height*0.95), cv::FONT_HERSHEY_PLAIN, 1.0, white);
-        }
-        cv::imshow("", black_img);
+        // if(calibrated) {
+        //     remap(black_img, black_img, map1, map2, cv::INTER_LINEAR);
+        //     cv::rectangle(black_img, cv::Rect(0, 0, img_size.width, img_size.height), green*0.5, 10);
+        //     cv::putText(black_img, "ESC to finish", cv::Point(img_size.width*0.05, img_size.height*0.95), cv::FONT_HERSHEY_PLAIN, 1.0, white);
+        // } else {
+            black_img_1 += detected_img_1;
+            black_img_2 += detected_img_2;
+            // cv::rectangle(black_img, cv::Rect(0, 0, img_size.width, img_size.height), red*0.8, 10);
+            // cv::putText(black_img, "Collecting images... press SPACE to perform calibration", cv::Point(img_size.width*0.05, img_size.height*0.95), cv::FONT_HERSHEY_PLAIN, 1.0, white);
+            // cv::putText(black_img, board_info, cv::Point(img_size.width*0.05, img_size.height*0.05), cv::FONT_HERSHEY_PLAIN, 1.0, white);
+            // str_maker.str("");
+            // str_maker << image_points.size();
+            // cv::putText(black_img, str_maker.str(), cv::Point(img_size.width*0.95, img_size.height*0.95), cv::FONT_HERSHEY_PLAIN, 1.0, white);
+        // }
+        cv::imshow("camera 1", black_img_1);
+        cv::imshow("camera 2", black_img_2);
         char c = cv::waitKey(1);
         if(c == 32) {
             yInfo() << "calibrating...";
@@ -221,12 +260,12 @@ public:
         dist_coeffs = cv::Mat::zeros(8, 1, CV_64F);
 
         // call calibrate camera
-        double rms = cv::calibrateCamera(object_points, image_points, img_size,
+        double rms = cv::calibrateCamera(object_points, image_points, img_size_1,
                                          camera_matrix, dist_coeffs, rvecs, tvecs, cv::CALIB_USE_LU | cv::CALIB_FIX_K3);
 
         cv::initUndistortRectifyMap(
             camera_matrix, dist_coeffs, cv::Mat(),
-            cv::getOptimalNewCameraMatrix(camera_matrix, dist_coeffs, img_size, 1, img_size, 0), img_size,
+            cv::getOptimalNewCameraMatrix(camera_matrix, dist_coeffs, img_size_1, 1, img_size_1, 0), img_size_1,
             CV_16SC2, map1, map2);
 
         std::cout << camera_matrix << std::endl;
@@ -237,8 +276,8 @@ public:
     {
         writer << "[CAMERA_CALIBRATION]" << std::endl;
         writer << std::endl;
-        writer << "w " << img_size.width << std::endl;
-        writer << "h " << img_size.height << std::endl;
+        writer << "w " << img_size_1.width << std::endl;
+        writer << "h " << img_size_1.height << std::endl;
         writer << "fx " << camera_matrix.at<double>(0, 0) << std::endl;
         writer << "fy " << camera_matrix.at<double>(1, 1) << std::endl;
         writer << "cx " << camera_matrix.at<double>(0, 2) << std::endl;
