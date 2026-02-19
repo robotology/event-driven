4734c80d6446d70b90ab9dad7cc2e1a676490e4b
Luna Gava <luna.gava@iit.it>
2026-02-19 14:04:49 +0000

    chainsae,aed-sae-aae log2vid

--- cpp_tools/log2vid/log2vid.cpp
+++ cpp_tools/log2vid/log2vid.cpp
@@ -160,7 +160,89 @@ int main(int argc, char* argv[])
             std::cout << "\r" << std::fixed << std::setprecision(1) << virtual_timer << " s / " << loader.getLength() << " s       ";
             std::cout.flush();
         }
-    } else {
+    } else if(rf.check("chainsae")) {
+        ev::chainSAE chainsae;
+        chainsae.initialise(res); 
+
+        while(loader.incrementReadTill(virtual_timer) && !stampfile.eof()) {
+            cv::Mat img, img8U;
+
+            for (ev::offlineLoader<ev::AE>::iterator v = loader.begin(); v != loader.end(); v++) {
+                chainsae.update(v->x, v->y, v.timestamp(), v->p);
+            }
+            
+            cv::Mat img64f = chainsae.getSurface();
+            img64f.convertTo(img8U, CV_8U, 255);
+
+            img8U = 255 - img8U;
+            cv::cvtColor(img8U, img, cv::COLOR_GRAY2BGR);
+
+            if(vis) {
+                cv::imshow("vLog2vid", img);
+                cv::waitKey(1);
+            }
+            dw << img;
+            if(stampfile.is_open()) stampfile >> virtual_timer;
+            else virtual_timer += period;
+            std::cout << "\r" << std::fixed << std::setprecision(1) << virtual_timer << " s / " << loader.getLength() << " s       ";
+            std::cout.flush();
+        }
+    } else if(rf.check("aedsae")) {
+
+        ev::AEDSAE aedsae;
+        aedsae.initialise(res, rf.check("deltat", Value(0.3)).asFloat64(), rf.check("lambda", Value(0.3)).asFloat64()); 
+        while(loader.incrementReadTill(virtual_timer) && !stampfile.eof()) {
+            cv::Mat img, img8U;
+
+            for (ev::offlineLoader<ev::AE>::iterator v = loader.begin(); v != loader.end(); v++) {
+                aedsae.update(v->x, v->y, v.timestamp(), v->p);
+            }
+            
+            cv::Mat img64f = aedsae.getSurface();
+            img64f.convertTo(img8U, CV_8U, 255);
+
+            img8U = 255 - img8U;
+            cv::cvtColor(img8U, img, cv::COLOR_GRAY2BGR);
+
+            if(vis) {
+                cv::imshow("vLog2vid", img);
+                cv::waitKey(1);
+            }
+            dw << img;
+            if(stampfile.is_open()) stampfile >> virtual_timer;
+            else virtual_timer += period;
+            std::cout << "\r" << std::fixed << std::setprecision(1) << virtual_timer << " s / " << loader.getLength() << " s       ";
+            std::cout.flush();
+        }
+    } else if(rf.check("aae")) {
+
+        ev::AAE aae;
+        aae.initialise(res, rf.check("bs", Value(15)).asInt32()); 
+        while(loader.incrementReadTill(virtual_timer) && !stampfile.eof()) {
+            cv::Mat img, img8U;
+
+            for (ev::offlineLoader<ev::AE>::iterator v = loader.begin(); v != loader.end(); v++) {
+                aae.update(v->x, v->y, v.timestamp(), v->p);
+            }
+            
+            cv::Mat img64f = aae.getSurface();
+            img64f.convertTo(img8U, CV_8U, 255);
+
+            img8U = 255 - img8U;
+            cv::cvtColor(img8U, img, cv::COLOR_GRAY2BGR);
+
+            if(vis) {
+                cv::imshow("vLog2vid", img);
+                cv::waitKey(1);
+            }
+            dw << img;
+            if(stampfile.is_open()) stampfile >> virtual_timer;
+            else virtual_timer += period;
+            std::cout << "\r" << std::fixed << std::setprecision(1) << virtual_timer << " s / " << loader.getLength() << " s       ";
+            std::cout.flush();
+        }
+    }
+    else {
 
         //initialise iso_drawer
         double duration = rf.check("window", Value(0.5)).asFloat64();
